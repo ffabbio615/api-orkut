1df2d9f57d4d4a8d83504a7e1e81b2669c6535dd
ffabbio615 <fabiomarques.ads@gmail.com>
2026-04-10 19:48:22 -0300

    Adição novamente do SSL para o render

--- src/config/db.js
+++ src/config/db.js
@@ -8,6 +8,16 @@ const {Pool} = require('pg');
 // })
 
 
+// module.exports = new Pool({
+//     connectionString: process.env.DATABASE_URL
+// });
+
+
+const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === "production";
+
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
